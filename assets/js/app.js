7c339cb2e10451320534faedd3a0c46d9dfee135
LorenzoTesoro <lorenzotesoro2@gmail.com>
2023-02-05 09:45:30 +0100

    add call api

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,24 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      message: "hello!",
+      quote: null,
+      api_base_url: "https://api.adviceslip.com/advice",
     };
+  },
+  methods: {
+    getRandomAdvice(url) {
+      axios
+        .get(url)
+        .then((response) => {
+          console.log(response.data.slip.advice);
+          this.quote = response.data.slip.advice;
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    },
+  },
+  mounted() {
+    this.getRandomAdvice(this.api_base_url);
   },
 }).mount("#app");
